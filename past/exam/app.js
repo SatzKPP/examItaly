431e649e612d6ce51919b318df3eaed28ef53020
Satz <satzkpp@gmail.com>
2025-06-07 19:07:32 +0900

    Update app.js

--- past/exam/app.js
+++ past/exam/app.js
@@ -1,21 +1,3 @@
-let solution_view = false;
-
-function switch_solution_view(){
-    solution_pdf = document.getElementById("solution_pdf");
-    button = document.getElementById("button");
-    if (solution_view == false) {
-        solution_pdf.classList.remove("noview");
-        button.innerHTML = "非表示"
-        solution_view = true;
-
-    } else {
-        solution_pdf.classList.add("noview");
-        button.innerHTML = "表示"
-        solution_view = false;
-    }
-}
-
-
 document.addEventListener("DOMContentLoaded", function() {
     if (window.location.hash) {
       const target = document.querySelector(window.location.hash);
